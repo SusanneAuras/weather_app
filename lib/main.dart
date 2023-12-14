2d8741adc95c06b8e9c25edc008e43c5ef7bbf66
Susanne Auras <susanne.auras1@gmail.com>
2023-12-14 15:26:46 +0100

    final

--- lib/main.dart
+++ lib/main.dart
@@ -6,71 +6,51 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
-  get appBar => null;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: WeatherApp(
-        Scaffold(
-          bottomSheet: ,
-          appBar: ,
-          drawerDragStartBehavior: ,
-          drawer: ,
-          bottomNavigationBar: ,
-          floatingActionButton: ,
-          backgroundColor: ,
-          body: Container(
-            child: AppBar ,
-            height: ,
-            width: ,
-            color:,
-          ),),),);
-  }
-        
-
-}
-          
-          
-          
-             TextStyle(
-              backgroundColor: 
-              fontStyle: 
-              fontWeight: 
-              decoration: 
-              shadows:
-              color: Colors.blue,
-              fontSize: 24),
-
-
-              child: const Text(
-              style: ,
-              maxLines: ,
-              overflow: ,
-              textDirection: ,
-              textHeightBehavior: ,
-              selectionColor: ,
-              "Willkommen zur Wetter-App"),
-        
-          ),
-          appBar: AppBar(
-            backgroundColor: ,
-            title: const Text(
-              "Weather App Bar",
-            ),
-          ),;
-        ),
-      ),
+    return const MaterialApp(
+      home: WeatherApp(),
     );
   }
 }
 
 class WeatherApp extends StatelessWidget {
-  const WeatherApp(Scaffold scaffold, {super.key});
+  const WeatherApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        titleTextStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 48.0,
+            backgroundColor: Colors.orange),
+        title: const Text("Wetter-App"),
+      ),
+      body: const SizedBox(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Stadt: Hannover",
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold)),
+              Text(
+                "Temperatur: 4 Grad",
+                style: TextStyle(color: Colors.blue, fontSize: 24.0),
+              ),
+              Text(
+                "Wetter: winterlich",
+                style: TextStyle(color: Colors.blue, fontSize: 24.0),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
